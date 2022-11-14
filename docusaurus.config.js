25050dcfbe6308522136043b76bc6dd91e112d01
EugeneWay <relax211@gmail.com>
2022-11-14 17:25:26 +0100

    add main page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Vara Documentation Portal',
+  title: 'Vara Network Documentation Portal',
   tagline: 'All documentation related to Vara Network',
   url: 'https://wiki.vara-network.io/',
   baseUrl: '/',

c20a929615e8649002ed65d9467dd965789f75b5
ereburg <gimmethenick@gmail.com>
2024-06-07 14:00:38 +0300

    remove unused blog pages; bump dependencies; add iframe-resizer script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,4 +91,15 @@ export default {
         },
       },
     }),
+
+  scripts: [
+    // {
+    //   src: 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.9/iframeResizer.contentWindow.min.js',
+    //   defer: true,
+    // },
+    {
+      src: 'https://cdn.jsdelivr.net/npm/@iframe-resizer/child',
+      defer: true,
+    },
+  ],
 };

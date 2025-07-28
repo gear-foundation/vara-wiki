6d5896bbe3f73955408200c3fde5b1e2510ad00d
EugeneWay <relax211@gmail.com>
2025-07-28 17:39:57 +0200

    add dynamic addresses

---
+++ src/addresses/bridge-addresses.js
@@ -0,0 +1,26 @@
+// src/addresses.js
+
+// Ethereum (Hoodi testnet) contract addresses
+export const eth = {
+    verifier: "0xC801bC5af5531A7EcF956741ed42ccF091686823",
+    messageQueue: "0x91ED0D3519aCE35d0591989e28735Cb1f4791924",
+    bridgingPayment: "0x3722A335F51aa638ce730F9E029FE44862ed6dc7",
+    usdc: "0x07fcAcC4d25F642482C2Ab7EBBEf1D9FE7F89E86",
+    usdt: "0xbc24436a5AF9b58431F6385703C15FdA8c60d1f5",
+    WETH: "0x6f47561Be156E185572B0eaDd1D11Bc66758E44e",
+    wtvara: "0xD07F9259C9D80935920Eed57071Cdd3F2026FD21",
+    erc20Manager: "0xCc1C2f83b40f1FAb06753Ea4e4119EB2083a5EFe",
+  };
+  
+  // Vara (Testnet) contract addresses
+  export const vara = {
+    checkpointLightClient: "0xdb7bbcaff8caa131a94d73f63c8f0dd1fec60e0d263e551d138a9dfb500134ca",
+    ethEventsElectra: "0x0e81a4201bdb78fb313aa01a7764aa2c6bd254463026dc700db9f657c54d47b1",
+    historicalProxy: "0x5d2a0dcfc30301ad5eda002481e6d0b283f81a1221bef8ba2a3fa65fd56c8e0f",
+    vftManager: "0xa1497efdb24b1f08a5801b1c59610c389a5341b4db7367a7ec70fa6f9b9a2a1c",
+    bridgingPayment: "0xa62a5bb70342147a67170777caa76ca2e0fe5fb413a9c687fbf8ee8002bde6b5",
+    wusdc: "0xf83bfe546c7115c403308faf5f33f4550c1ff4e429c6469ca33aa6740cf883c2",
+    wusdt: "0x7288022b85e4e7563e4f3d3ae7f443477f8ce1342d204484a28818949e0371cd",
+    weth: "0x27dd13d45898315524edbe3447f8473d83d91a2df5d06f205d27d86d58b71c41",
+    tokenizedVara: "0x47cd3c611f9412f936dc60430b574b048cd211bdfac8259e2ad5af735b6059b4",
+  };

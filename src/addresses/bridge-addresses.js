63d6f332fb151b85cb63020ec138d3dda8fd8666
Eugene Way <relax211@gmail.com>
2025-10-07 13:15:26 +0200

    Update bridge addresses (#266)

--- src/addresses/bridge-addresses.js
+++ src/addresses/bridge-addresses.js
@@ -1,15 +1,17 @@
 // Ethereum (Hoodi testnet) contract addresses
 export const eth = {
-  verifier: "0x80c9a727fdeadfa48864a4fb9bf7bb3e4e40ca52",
-  messageQueue: "0x459dbec00b9577500d7830a31051174061c2818e",
+  verifier: "0xc3ac0c364452acEE4366CD088F947965ec486e8F",
+  messageQueue: "0xAb8F315Cc80cf2368750fE5A33E259d6241b3dEB",
   messageQueueProxy: "0x55B97F9229dc310837A880c7898f7d411528cC6d",
-  bridgingPayment: "0x27370c574d4aE6B85A2F45a6899C0cD59E59B38E",
-  usdc: "0xb92e77de4e3f32116cf0d53b882c30a1f73b641e",
-  usdt: "0x09e1687c3512c8507bcca8bf55b131dfae9b68c7",
-  WETH: "0xdb7057418e9272216551a6ea07876a1e26d306f9",
-  wtvara: "0xD8B16a9bBA9AeBBe77F89528231083ED983c76fC",
-  erc20Manager: "0xc06450d5a251a9434571402472c6f4f3acb4521c",
+  bridgingPayment: "0x512Ac5b5f0830d1C5CB44741a5c0a0E5B66696FD",
+  usdc: "0x263898d2f6f8E153F1e4DD4CAEF86C93784fCf33",
+  usdt: "0x7728A33EBEBCfa852cf7f7Fc377BfC87C24a701A",
+  WETH: "0xE0decAa66aED871ac9eb924443D1Bf333Fdb062E",
+  wtvara: "0xE1ab85A8B4d5d5B6af0bbD0203EB322DF33d0464",
+  erc20Manager: "0xA17187De490dB5F7160822dA197bcAc39d64baCb",
   erc20ManagerProxy: "0xD2a0951DBCfdA8de36Ab315A57C30F0784d01342",
+  governanceAdmin: "0x3288b744ADbC25f07b636718d72ec07F8C77E5E9",
+  governancePauser: "0x384B68Fc08c59Df51694f7F6A24A112B8331eEb0",
 };
 
 // Vara (Testnet) contract addresses
@@ -17,10 +19,10 @@ export const vara = {
   checkpointLightClient: "0xdb7bbcaff8caa131a94d73f63c8f0dd1fec60e0d263e551d138a9dfb500134ca",
   ethEventsElectra: "0x0e81a4201bdb78fb313aa01a7764aa2c6bd254463026dc700db9f657c54d47b1",
   historicalProxy: "0x5d2a0dcfc30301ad5eda002481e6d0b283f81a1221bef8ba2a3fa65fd56c8e0f",
-  vftManager: "0x0cbd1f152bb47dcd0a95ca0dde194978b61b1faebe1df09b5e2a1f6e8e5f5fa1",
-  bridgingPayment: "0x2912fde38a646f627e31cde70307ca0006fae14520ff62ffed90b22c90a95a6c",
-  wusdc: "0x54ec9739ae960b250864644b1f9b9190838183df4d53fd6aba40173867f440f2",
-  wusdt: "0x5031bfe111a3bb1c53cb24b84070f8d506ab07c73f6a7e46c72e0ad38be4422b",
-  weth: "0xfe5890ea569b6b86e9226dce1cf6e9a714cad0fc0ea5f06375f1767e9a7e4083",
-  tokenizedVara: "0xee354b7cb70e31b45928f5a767025d10338181bd9a97a1524061cf33489c6bb1",
+  vftManager: "0x39514728b5828eccad02b8f1183e7ccea43849157ed6504c38b28420a85f0d12",
+  bridgingPayment: "0x8b86f8cb74f2a0050bd2207efa22cdb59ce20051126618e2ebcacfe49c7e1e4c",
+  wusdc: "0x9f332e61589e0850dce6d8e6070ea5618de33d9f134a4a35d6d1164dc9002f48",
+  wusdt: "0x464511231a1afe9108a689ed3dbbb047ca308d6f5dfb86453e4df5612a2d668a",
+  weth: "0xba764e2836b28806be10fe6f674d89d1e0c86898d25728f776588f03bddc6f58",
+  tokenizedVara: "0xd0f89cfd994c92bb743a5a69049609b796e2026e05318f7eef621a5e31df3d4b",
 };

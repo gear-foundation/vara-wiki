6e5d2a36eabdf4aabf933d80a09a2417d82dd50b
Eugene Way <relax211@gmail.com>
2025-10-27 17:35:29 +0000

    Add mainnet bridge addresses (#267)

--- src/addresses/bridge-addresses.js
+++ src/addresses/bridge-addresses.js
@@ -14,6 +14,20 @@ export const eth = {
   governancePauser: "0x384B68Fc08c59Df51694f7F6A24A112B8331eEb0",
 };
 
+// Ethereum (Mainnet) contract addresses
+export const ethMainnet = {
+  verifier: "0xDdAAC7F0814368D58D40216C6391a5e40A8cd47E",
+  messageQueue: "0x8E01Fbf136cA97627ca241dB9EFf1DFE3f2195F6",
+  bridgingPayment: "0x03Dd51eeE793CE5523D28752f3019B0c9DfeE6C5",
+  usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+  usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+  WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  wvara: "0xB67010F2246814e5c39593ac23A925D9e9d7E5aD",
+  erc20Manager: "0x16fCff97822fcf3345Fa76D29c229b11C49EaE12",
+  governanceAdmin: "0x3681A3e25F5652389B8f52504D517E96352830C3",
+  governancePauser: "0x257936C55518609E47eAab53f40a6e19437BEF47",
+};
+
 // Vara (Testnet) contract addresses
 export const vara = {
   checkpointLightClient: "0xdb7bbcaff8caa131a94d73f63c8f0dd1fec60e0d263e551d138a9dfb500134ca",
@@ -25,4 +39,16 @@ export const vara = {
   wusdt: "0x464511231a1afe9108a689ed3dbbb047ca308d6f5dfb86453e4df5612a2d668a",
   weth: "0xba764e2836b28806be10fe6f674d89d1e0c86898d25728f776588f03bddc6f58",
   tokenizedVara: "0xd0f89cfd994c92bb743a5a69049609b796e2026e05318f7eef621a5e31df3d4b",
+};
+
+// Vara (Mainnet) contract addresses
+export const varaMainnet = {
+  checkpointLightClient: "0xf0a411ed5b8c28194d60781306de2ef131ec7df28658f7a857fc95fd86db7e8b",
+  historicalProxy: "0x21b82d3ee72e0a5ac81db100c0a703050593b551c50e86b18c9da0be793660fb",
+  vftManager: "0xe01ddc667f80cf57704352b557668b710c345395abcac0752c01402d16e3e81b",
+  bridgingPayment: "0xcc1901de1f8134ed2c2d30775e4840084ad5d527cfcbf63c3247df24a2e3075e",
+  wusdc: "0xd1de816d7dce6439504552686ab333e5b7302b1549763656b30af1f8a5871b6a",
+  wusdt: "0x4255ff4a87a4c13dc39f74ace8c4948bbef2f75fb639d66639a1cfcc99e6243e",
+  weth: "0xde45bdbb0345919a11561d43a5082e0b25061d4a2c6eb80009c1cfbccb80d0de",
+  tokenizedVara: "0xdbf80fe5bd78b44510762770a14dc2a5b13a6bb167ff12c2edcc7ca3deadc16d",
 };

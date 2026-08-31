5b171c6bbc65e513cabf1aae57a8a6abde823e0f
AndrePanin <ap@gear-tech.io>
2026-08-31 16:23:55 +0400

    Update testnet info (#317)
    Co-authored-by: VibeMyCode Bot <bot@vibemycode.dev>

--- src/addresses/bridge-addresses.js
+++ src/addresses/bridge-addresses.js
@@ -1,20 +1,3 @@
-// Ethereum (Hoodi testnet) contract addresses
-export const eth = {
-  verifier: "0xc3ac0c364452acEE4366CD088F947965ec486e8F",
-  messageQueue: "0xAb8F315Cc80cf2368750fE5A33E259d6241b3dEB",
-  messageQueueProxy: "0x55B97F9229dc310837A880c7898f7d411528cC6d",
-  bridgingPayment: "0x512Ac5b5f0830d1C5CB44741a5c0a0E5B66696FD",
-  usdc: "0x263898d2f6f8E153F1e4DD4CAEF86C93784fCf33",
-  usdt: "0x7728A33EBEBCfa852cf7f7Fc377BfC87C24a701A",
-  WETH: "0xE0decAa66aED871ac9eb924443D1Bf333Fdb062E",
-  wbtc: "0xa56a332d34b2db33ebc41dc0194afd28cb20d19b",
-  wtvara: "0xE1ab85A8B4d5d5B6af0bbD0203EB322DF33d0464",
-  erc20Manager: "0xA17187De490dB5F7160822dA197bcAc39d64baCb",
-  erc20ManagerProxy: "0xD2a0951DBCfdA8de36Ab315A57C30F0784d01342",
-  governanceAdmin: "0x3288b744ADbC25f07b636718d72ec07F8C77E5E9",
-  governancePauser: "0x384B68Fc08c59Df51694f7F6A24A112B8331eEb0",
-};
-
 // Ethereum (Mainnet) contract addresses
 export const ethMainnet = {
   verifier: "0xDdAAC7F0814368D58D40216C6391a5e40A8cd47E",
@@ -28,26 +11,6 @@ export const ethMainnet = {
   erc20Manager: "0x16fCff97822fcf3345Fa76D29c229b11C49EaE12",
   governanceAdmin: "0x3681A3e25F5652389B8f52504D517E96352830C3",
   governancePauser: "0x257936C55518609E47eAab53f40a6e19437BEF47",
-};
-
-// Vara (Testnet) contract addresses
-export const vara = {
-  checkpointLightClient:
-    "0xdb7bbcaff8caa131a94d73f63c8f0dd1fec60e0d263e551d138a9dfb500134ca",
-  ethEventsElectra:
-    "0x0e81a4201bdb78fb313aa01a7764aa2c6bd254463026dc700db9f657c54d47b1",
-  historicalProxy:
-    "0x5d2a0dcfc30301ad5eda002481e6d0b283f81a1221bef8ba2a3fa65fd56c8e0f",
-  vftManager:
-    "0x39514728b5828eccad02b8f1183e7ccea43849157ed6504c38b28420a85f0d12",
-  bridgingPayment:
-    "0x8b86f8cb74f2a0050bd2207efa22cdb59ce20051126618e2ebcacfe49c7e1e4c",
-  wusdc: "0x9f332e61589e0850dce6d8e6070ea5618de33d9f134a4a35d6d1164dc9002f48",
-  wusdt: "0x464511231a1afe9108a689ed3dbbb047ca308d6f5dfb86453e4df5612a2d668a",
-  weth: "0xba764e2836b28806be10fe6f674d89d1e0c86898d25728f776588f03bddc6f58",
-  wbtc: "0xc1ec06d99efcffd863f9c2ad2bc76f656aff861acf06f438046c64e5b41e3fd9",
-  tokenizedVara:
-    "0xa1a37e5a36e8a53921f6bedefadec91dc510636079a22238e9edf8233aaa494e",
 };
 
 // Vara (Mainnet) contract addresses
